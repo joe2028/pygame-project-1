dad7f1cb024f6b8f595d58f9b5e9a78b5f3f4d45
joe2028 <96135184+joe2028@users.noreply.github.com>
2021-12-17 23:28:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,81 +1,44 @@
-import pgzrun as pyg
-from random import *
+import pgzrun as pg
+import os
 import time
+from random import *
 
-alien = Actor('alien')
-WIDTH = 1000
-HEIGHT = alien.height + 500
-alien.center
+egg = Actor('rene')
+bird = Actor('bird')
+WIDTH = 500
+HEIGHT = 500
+bird.center
 score = 0
 game_over = False
 
-egg = Actor('rene')
-egg.center = (randint(25, 500), randint(50, 500))
 
-startTime = time.time()
-time_elasped = 0
+bird_y = 200
+bird_y_speed = 0
 
-def check_egg_collection():
-    global alien, egg, score
-    if(egg.colliderect(alien)):
-        score += 1
-        egg.center = (randint(0, 1000), randint(0, 685))
-
-
-    #if game_over == True:
-       # screen.draw.text('GAME OVER!!!', topright = (100, 50), fontsize = 64, color="black")
-       # sounds.damndaniel.play()
-    else:
-        if keyboard.left:
-            alien.x -= 5
-            if alien.x <= 0:
-                alien.right = 1000
-        elif keyboard.right:
-            alien.x += 5
-            if alien.right > WIDTH:
-                alien.left = 0
-        elif keyboard.up:
-            alien.y -= 5
-            if alien.y <= 0:
-                alien.y = 300
-        elif keyboard.down:
-            alien.y += 5
-            if alien.y >= 683 and alien.x >= 5:
-                alien.y = 0
-                sounds.scary.play()
-
-if game_over == True:
-        screen.draw.text('GAME OVER!!!', topright = (100, 50), fontsize = 64, color="black")
-        sounds.damndaniel.play()
-
-
+def update(dt):
+    global bird_y, bird_y_speed
+    bird_y += 30 * dt
+    bird_y_speed += 516 * dt
+    bird_y += bird_y_speed * dt
+    bird_y = int(bird_y)
 
 def draw():
-    global score, egg
     screen.clear()
-    screen.fill((255, 255, 255))
-    if game_over == True:
-        screen.draw.text('GAME OVER!!!', topright = (100, 50), fontsize = 64, color="black")
-    screen.draw.text('Time left: ' + str(60 - time_elasped), topright = (380, 20), fontsize = 32, color="black")
-    screen.draw.text('Score: ' + str(score), topleft = (100, 50), fontsize = 32, color="black")
-    egg.draw()
-    alien.draw()
+    screen.fill((255,255,255))
+    screen.draw.filled_rect(
+        Rect(
+            (0,0),
+            (500, 500)
+            ),
+            color=(35, 92, 118)
+        )
+    bird.draw()
+    bird.y = bird_y
+    
+def on_key_down():
+    global bird_y_speed
+    if bird_y > 0:
+        bird_y_speed = -165
 
-def set_alien_normal():
-    alien.image = 'alien'
 
-def set_alien_hurt():
-    aline.image = 'alien_hurt'
-
-def update():
-    global time_elasped
-    time_elasped = int(time.time() - startTime)
-    check_egg_collection()
-    if score >= 100:
-        screen.draw.text('YOU WIN!', center = (500, 300), fontsize = 65, color="black")
-    if time_elasped >= 60:
-        game_over = True
-        screen.draw.text('GAME OVER!!!', topright = (100, 50), fontsize = 64, color="black")
-        sounds.damndaniel.play()
-
-pyg.go()
+pg.go()
